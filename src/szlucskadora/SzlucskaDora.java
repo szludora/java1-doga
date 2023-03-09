45a520b465c47e73f1f00071b30c1c238dbfca08
szludora <szlucska.dora@gmail.com>
2023-03-09 18:28:49 +0100

    metódus ciklussal, randommal

--- src/szlucskadora/SzlucskaDora.java
+++ src/szlucskadora/SzlucskaDora.java
@@ -1,9 +1,28 @@
 package szlucskadora;
+import java.util.Random;
+
 public class SzlucskaDora {
     
     // Szlucska Dóra
     
     public static void main(String[] args) {
+       
+    tombLetrehozas(4);
+    
+    static int[] tombLetrehozas(int hossz){
+        int[] veletlenek = null;
+        
+        int i = 0;
+        while (i < hossz) {
+        int veletlen = (int)(Math.random()*16) - 5;
+        veletlenek.add(veletlen);
+        i++;}
+        return veletlenek;
+        
+    }
+        
+                
+    
     }
     
 }
